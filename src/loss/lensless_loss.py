98deddb3284ad7d7715940daf4de58bb77b1b074
German Zverev <German.soft@mail.ru>
2026-06-11 15:18:19 +0300

    Minor changes

--- src/loss/lensless_loss.py
+++ src/loss/lensless_loss.py
@@ -14,5 +14,11 @@ class LenslessLoss(nn.Module):
         self.loss_lpips = LearnedPerceptualImagePatchSimilarity(net_type = "vgg", reduction = "mean", normalize = True).to(device)
 
     def forward(self, output, lensed, **kwargs):
-        loss = self.loss_mse(output, lensed) + self.loss_lpips(output.clamp(0, 1), lensed)
-        return {"loss": loss}
+        mse_loss = self.loss_mse(output, lensed)
+        lpips_loss = self.loss_lpips(output.clamp(0, 1), lensed)
+        loss = mse_loss + lpips_loss
+        return {
+            "loss": loss,
+            "mse": mse_loss,
+            "lpips": lpips_loss
+        }
