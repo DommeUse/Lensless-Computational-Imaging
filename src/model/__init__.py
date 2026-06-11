831a6cbbe66a53ec84034aafa8dc7aa10d9060d3
German Zverev <German.soft@mail.ru>
2026-06-11 14:42:43 +0300

    Updated init in model dir

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,13 +1,1 @@
-from transformers import AutoConfig, AutoModel
-
-from src.model.baseline_model import BaselineConfig, BaselineModel
-from src.model.resnet import MyResNetConfig, MyResNetModel
-
-
-def register_models():
-    # Register all models and their configs
-    AutoConfig.register(BaselineConfig.model_type, BaselineConfig)
-    AutoModel.register(BaselineConfig, BaselineModel)
-
-    AutoConfig.register(MyResNetConfig.model_type, MyResNetConfig)
-    AutoModel.register(MyResNetConfig, MyResNetModel)
+from src.model.admm import ADMM
