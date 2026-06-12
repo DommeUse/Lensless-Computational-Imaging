8dcb03afaf6f16be48bd9197657c88158f1d537e
German Zverev <German.soft@mail.ru>
2026-06-12 12:59:02 +0300

    Returned utils without accelerator

--- src/utils/init_utils.py
+++ src/utils/init_utils.py
@@ -28,28 +28,20 @@ def set_worker_seed(worker_id):
     random.seed(worker_seed)
 
 
-def set_random_seed(
-    seed, device_specific=True, deterministic=True, strict_deterministic=False
-):
+def set_random_seed(seed):
     """
     Set random seed for model training or inference.
 
     Args:
         seed (int): defines which seed to use.
-        device_specific (bool): if True, set unique seed for each process.
-        deterministic (bool): If True, enable deterministic behavior where possible (cudnn).
-        strict_deterministic (bool): If True, require deterministic PyTorch algorithms globally.
     """
-    accelerator_deterministic = deterministic and strict_deterministic
-    set_seed(
-        seed, device_specific=device_specific, deterministic=accelerator_deterministic
-    )
-    if not accelerator_deterministic:
-        if deterministic and (not strict_deterministic):
-            torch.backends.cudnn.deterministic = True
-            # benchmark=True works faster but reproducibility decreases
-            torch.backends.cudnn.benchmark = False
-
+    # fix random seeds for reproducibility
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    # benchmark=True works faster but reproducibility decreases
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
 
 
