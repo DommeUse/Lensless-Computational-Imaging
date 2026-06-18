e75321f7aa45a38cb0930f810573c9279e0dd172
German Zverev <German.soft@mail.ru>
2026-06-18 14:18:07 +0300

    Added file with checkpoints ids

---
+++ model_ids.py
@@ -0,0 +1,6 @@
+MODEL_IDS = {
+    "admm_unrolled": ("1csgtfyet3bDzs_zcfw2t7cSoV-MDNXpX", "admm_unrolled"),
+    "pre": ("1OXBc_GdCbdgSVfhIinwJKgpi8q7CFMqZ", "modular_pre"),
+    "post": ("1HC8nETZpg_4GBXIWoODYZGz7BjfvEDik", "modular_post"),
+    "pre-post": ("1cz_dO90ohu0v5xpdIhR-6FKEKK_Rr8_5", "modular_pre-post"),
+}
